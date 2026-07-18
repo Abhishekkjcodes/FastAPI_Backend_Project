55f58cf340c500bfd78cf0de4e5b04af45ef5a6e
Abhishekkjcodes <155182094+Abhishekkjcodes@users.noreply.github.com>
2026-07-18 18:58:39 +0530

    modified alembic env.py

--- alembic/env.py
+++ alembic/env.py
@@ -9,7 +9,7 @@ from app.configs import settings
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url",f"postgresql+psycopg://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}")
+config.set_main_option("sqlalchemy.url",f"postgresql+psycopg://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}?sslmode=require&channel_binding=require")
 #?sslmode=require&channel_binding=require add for neon
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
